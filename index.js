da7ad127709731f98cd12d05d61070092c4ae1a8
Syz03 <aakibdiscoo@gmail.com>
2021-07-02 20:51:09 +0545

    Updates with animation in the page

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-anime({
-  targets: "div",
-  translateX: 250,
-  rotate: "1turn",
-  backgroundColor: "#FFF",
-  duration: 800,
-});
+
+// function ValidateEmail() {
+
+// var btn1 = document.getElementById("text").value;
+
+// var regx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value);
+//  if (regx.text(btn1)){
+//     alert("Valid email");
+//     return (true);
+//   }
+//     alert("You have entered an invalid email address!");
+//     // return (false)
+// }
+
+// anime({
+//   targets: '.div .el',
+//   translateX: 250
+// });
+
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
+// // ..
+// AOS.init();
